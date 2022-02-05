5f7495d7c7c4c17b3e940bc3b8c109c1203ec0f1
Davit Gujejiani <66127957+DavitGujejiani@users.noreply.github.com>
2022-02-06 00:36:56 +0400

    tags list method finished

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -4,11 +4,37 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
 class TagsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // todo: return tags
+        $filters = [
+            'sort'  => $request->input('sort') ?? 'article_count',
+            'order' => $request->input('order') ?? 'desc',
+        ];
+
+        // fetch tags data and turn it into collection
+        $tags = collect(Tag::withArticlesCount()->with('articles')->get());
+
+        // add the oldest article relation created_at attribute to tag collection for sorting purposes
+        foreach ($tags as $tag) {
+            $tag['created_at'] = $tag->articles?->first()?->created_at;
+        }
+
+        // sorting logic
+        $sortBy = $filters['sort'] === 'created_at' ? 'created_at' : 'articles_count';
+        $order = $filters['order'] === 'asc' ? 'asc' : 'desc';
+        $tags = $tags->sortBy([[$sortBy, $order]]);
+
+        // remove articles and created_at data form collection
+        foreach ($tags as $key => $tag) {
+            $tags[$key] = collect($tag)->forget(['articles', 'created_at']);
+        }
+
+        return $tags;
     }
 
     public function articles()
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -1,10 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     protected $hidden = ['pivot'];
+
+    public function scopeWithArticlesCount(Builder $query)
+    {
+        $query->withCount('articles');
+    }
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class)->oldest();
+    }
 }
